b8bd8dd96fde197e3a0b00a351925376d09f9454
ridhopujiono <ridho@smktjp.sch.id>
2022-04-18 22:30:12 +0700

    membuat component di app.js

--- public/js/app.js
+++ public/js/app.js
@@ -2114,6 +2114,8 @@ window.Vue = (__webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm.js
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', (__webpack_require__(/*! ./components/ExampleComponent.vue */ "./resources/js/components/ExampleComponent.vue")["default"]));
+Vue.component('header-component', Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/HeaderComponent.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+Vue.component('footer-component', Object(function webpackMissingModule() { var e = new Error("Cannot find module './components/FooterComponent.vue'"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
