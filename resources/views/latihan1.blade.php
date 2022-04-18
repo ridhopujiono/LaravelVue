78c18b0d3845065ef1f5fd12e515301e2a97b098
ridhopujiono <ridho@smktjp.sch.id>
2022-04-18 22:01:26 +0700

    1. composer require laravel/ui 2. php artisan ui vue 3. hapus boostrap jika perlu 4. npm install 5. npm install vue-loader@^15.9.7 --save-dev --legacy-peer-deps 6. npm run dev

---
+++ resources/views/latihan1.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Vue</title>
+</head>
+<style>
+    body{
+        font-family: sans-serif;
+        margin: 15px;
+    }
+</style>
+<body>
+    
+    <div id="app">
+        
+    </div>
+
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
